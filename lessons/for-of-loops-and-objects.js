0fcb9cbd3cb312b2ed718ee4d4ac4ca0ea1a156d
Andrew Virts <96750426+asvirts@users.noreply.github.com>
2023-02-04 16:05:49 -0600

    for of loops

--- lessons/for-of-loops-and-objects.js
+++ lessons/for-of-loops-and-objects.js
@@ -2,3 +2,11 @@ const colors = ['red','orange','yellow']
 for (var color of colors) {
     console.log(color);
 }
+
+const car = {
+    speed: 200,
+    color: "crimson"
+}
+console.log(Object.keys(car)) // ["speed", "color"]
+console.log(Object.values(car)) // [200, "crimson"]
+console.log(Object.entries(car)) // [ ['speed', 200], ['color', 'crimson'] ]
