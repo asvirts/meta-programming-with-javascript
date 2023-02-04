5a0cf9658ccc089f7cddfc6fa4b06c2c133d0482
Andrew Virts <96750426+asvirts@users.noreply.github.com>
2023-02-04 14:22:02 -0600

    folders

--- index.js
+++ index.js
@@ -87,33 +87,3 @@ function letterFinder(word, match) {
 }
 letterFinder([],[])
 letterFinder("cat","c")
-
-
-// Introduction to object-oriented programming
-let shoes = 100
-let stateTax = 1.2
-
-function totalPrice(shoes, tax) {
-    return shoes * tax
-}
-
-let toPay = totalPrice(shoes, stateTax)
-console.log(toPay)
-
-let purchase1 = {
-    shoes: 100,
-    stateTax: 1.2,
-    totalPrice: function() {
-        let calculation = purchase1.shoes * purchase1.stateTax
-        console.log("Total price:", calculation)
-    }
-}
-
-let bird = {
-    hasWings: true,
-    canFly: true,
-    hasFeathers: true
-}
-
-let eagle1 = Object.create(bird)
-console.log("eagle1:", eagle1)
